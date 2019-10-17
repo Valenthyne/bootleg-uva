48db45363ff275c183527be45b9a1e02a26bb80b
Drachy <bradfordm01@gmail.com>
2019-10-16 19:07:14 -0500

    Base files for dynamic programming exercises

---
+++ uva_10465.java
@@ -0,0 +1,13 @@
+// Code to solve UVA Online Judge problem #10465
+
+import java.util.*;
+
+public class uva_10465 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
---
+++ uva_787.java
@@ -0,0 +1,13 @@
+// Code to solve UVA Online Judge problem #787
+
+import java.util.*;
+
+public class uva_787 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
