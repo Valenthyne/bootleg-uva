872b3268d0f2de2621a4794089dcf3f2cc5bfd54
Drachy <bradfordm01@gmail.com>
2019-10-22 19:03:59 -0500

    Preliminary upload for these solutions

--- uva_10465.java
+++ uva_10465.java
@@ -1,12 +1,73 @@
 // Code to solve UVA Online Judge problem #10465
 
-import java.util.*;
+import java.util.Scanner;
 
 public class uva_10465 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+
+            // Setting up basic variables and structures
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int t = sc.nextInt();
+
+            int[] memo = new int[10001];
+
+            /*
+                This memo list will be considering subproblems
+                where if Homer has 
+            */
+
+            // Can't eat nothing at zero time
+            memo[0] = 0;
+
+            // Meat of the dynamic programming algorithm
+            for (int i = 1; i <= t; i++) {
+
+                int krusty;
+                int apu;
+
+                // Considering the case of eating Krusty burgers in the time allotted
+                if (i - m >= 0) {
+                    krusty = memo[i - m];
+                } else {
+                    krusty = -1;
+                }
+
+                if (krusty >= 0) {
+                    krusty++;
+                }
+
+                // Considering the case of eating Apu burgers in the time allotted
+                if (i - n >= 0) {
+                    apu = memo[i - n];
+                } else {
+                    apu = -1;
+                }
+
+                if (apu >= 0) {
+                    apu++;
+                }
+
+                // Compare which one is going to yield the most burger consumption
+                memo[i] = Math.max(krusty, apu);
+            }
+
+            if (memo[t] != -1) {
+                System.out.println(memo[t]);
+            } else {
+                int i = t;
+                while (memo[--i] == -1) {}
+                System.out.println(memo[i] + " " + (t - i));
+            }
+
+        }
+        
+        sc.close();
 
     }
 
--- uva_787.java
+++ uva_787.java
@@ -1,6 +1,8 @@
 // Code to solve UVA Online Judge problem #787
 
-import java.util.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class uva_787 {
 
@@ -8,6 +10,62 @@ public class uva_787 {
 
         Scanner sc = new Scanner(System.in);
 
-    }
+        BigInteger[][] memo = new BigInteger[200][2];
 
+        while (sc.hasNext()) {
+        // While loop for handling a single test case
+        	
+        	ArrayList<BigInteger> arrList = new ArrayList<>();
+        	
+        	
+	        while (sc.hasNextInt()) {
+	        
+	            int n = sc.nextInt();
+	            if (n == -999999) {
+	                break;
+	            }
+	            if (n == 0) {
+	            	arrList.add(BigInteger.ZERO);
+	            } else {
+	            arrList.add(BigInteger.valueOf(n));
+	        	}
+	
+	        }
+	        
+	        BigInteger[] values = arrList.toArray(new BigInteger[arrList.size()]);
+	        
+	        memo[0][0] = values[0];
+	        memo[0][1] = values[0];
+	        
+	        BigInteger prevMax = memo[0][0];
+	        BigInteger prevMin = memo[0][1];
+	        
+	        for (int i = 1; i < arrList.size(); i++) {
+		        BigInteger x = values[i];
+		        	
+		        if (x.compareTo(BigInteger.ZERO) == 1) {
+		        	memo[i][0] = x.max(x.multiply(prevMax));
+		        	memo[i][1] = x.min(x.multiply(prevMin));
+		        }
+		        
+		        if (x.compareTo(BigInteger.ZERO) == -1) {
+		        	memo[i][0] = memo[i - 1][0].max(x.max(x.multiply(prevMin)));
+		        	memo[i][1] = memo[i - 1][1].min(x.min(x.multiply(prevMax)));
+		        }
+		        
+		        if (x.compareTo(BigInteger.ZERO) == 0) {
+		        	memo[i][0] = x.max(prevMax);
+		        	memo[i][1] = BigInteger.ZERO;
+		        }
+		        		        
+		        prevMax = memo[i][0];
+		        prevMin = memo[i][1];
+	        
+	        }
+	        int n = arrList.size();
+	        System.out.println(memo[n - 1][0].max(memo[n - 1][1]));
+        
+    }
+        sc.close();
+    }
 }
