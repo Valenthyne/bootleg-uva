e2284460ce17a5bbc143fdff84161732c23cef15
Drachy <bradfordm01@gmail.com>
2019-10-25 16:07:19 -0500

    Initial upload of these problems (framework)

---
+++ uva_920.java
@@ -0,0 +1,16 @@
+// Code to solve UVA Online Judge problem #920
+
+import java.util.Scanner;
+
+public class uva_920 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+
+    }
+}
